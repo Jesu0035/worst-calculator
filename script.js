181d7c1111a1d99fe60dfcfc98a85d6b702c558d
Jesus lopez <jesu0035@stud.kea.dk>
2021-02-05 22:53:33 +0100

    second commit boulean

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ window.log ('load', settingUp);
 
 
 let clearListResults
-let 
+let calculate 
 
 function settingUp(){
     console.log('settingUp');
+document.querySelector('#calculate').addEventListener('click', clearListResults);
+document.querySelector('#results').addEventListener('click');
+
+
+
+
     readFirstNumber();
 }
+function calculate(){
+    console.log('calculate');
+}
 function clearListResults(){
+    document.querySelector('#results').addEventListener('click');
     console.logd('clear results');
 }
 function readFirstNumber(){
@@ -17,10 +27,22 @@ readSecondNumber();
 }
 function readSecondNumber(){
     console.log('read second Number');
-    readOperator();
+    Operator();
 }
-function readOperator(){
+function Operator(){
     console.log('read operator');
+
+
+    
+  
+    if (Operator.value === "add")
+
+
+
+document.getElementById('#firstnumber').value;
+document.getElementById('#secondnumber').value;
+document.getElementById('#results').innerHTML =num1* num2;
+
     checkResultRounded();
 
 }
