c204379b2406f17783bb104c065eca0475d7cd97
Jesus lopez <jesu0035@stud.kea.dk>
2021-02-05 15:09:08 +0100

    first commit functions

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-script.js;
+window.log ('load', settingUp);
+
+
+let clearListResults
+let 
+
+function settingUp(){
+    console.log('settingUp');
+    readFirstNumber();
+}
+function clearListResults(){
+    console.logd('clear results');
+}
+function readFirstNumber(){
+    console.log('read first number');
+readSecondNumber();
+}
+function readSecondNumber(){
+    console.log('read second Number');
+    readOperator();
+}
+function readOperator(){
+    console.log('read operator');
+    checkResultRounded();
+
+}
+function checkResultRounded(){
+    console.log('result rounded');
+    resultFirstNumberField();
+
+}
+function resultFirstNumberField(){
+    console.log('result fist number');
+    resultEndList();
+}
+function resultEndList (){
+    console.log('result end list');
+    scrollToButtom();
+}
